cad053bb6de3e7aa47156deb0801f92fa87f5317
Wataru Kume <>
2018-07-03 22:06:41 +0900

    make two classes before provide module

---
+++ chapter07/module.rb
@@ -0,0 +1,29 @@
+class Product
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+
+  private
+    def log(text)
+      puts "[LOG] #{text}"
+    end
+end
+
+class User
+  def name
+    log 'name is called'
+    'Alice'
+  end
+
+  private
+    def log(text)
+      puts "[LOG] #{text}"
+    end
+end
+
+product = Product.new
+p product.title
+
+user = User.new
+p user.name
