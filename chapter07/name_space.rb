a80327b270f5f0c87affb3f0884e7cb63f98b960
Wataru Kume <>
2018-07-04 23:35:43 +0900

    name space

---
+++ chapter07/name_space.rb
@@ -0,0 +1,21 @@
+module Baseball
+  class Second
+
+    def initialize(player, uniform_number)
+      @player = player
+      @uniform_number = uniform_number
+    end
+  end
+end
+
+module Clock
+  class Second
+    def initialize(digits)
+      @digits = digits
+    end
+  end
+end
+
+p Baseball::Second.new('Alice', 13)
+
+p Clock::Second.new(13)
